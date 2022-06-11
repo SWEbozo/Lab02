42f85e580b9a38f306fdab1752641fa32c3d8607
Pratham Saldhi <63696229+psaldhi@users.noreply.github.com>
2022-06-10 21:24:14 -0700

    Update Hello.java
    Name and Student id added.

--- Hello.java
+++ Hello.java
@@ -7,7 +7,7 @@ public class Hello {
 		// Add a new line with your information
 		System.out.println("216481604 - This line was added by Omar Martinez.");
 		System.out.println("301814734 - This line was added by Omar Ibrahim.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("301603666 - This line was added by Pratham Saldhi.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
