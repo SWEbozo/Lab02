0290507e6124ce9e803aca7db82e28c7a1b8a8a0
SWEbozo <oamartinez@csus.edu>
2022-06-12 21:30:31 -0700

    fixed earlier deletion

--- Hello.java
+++ Hello.java
@@ -8,7 +8,7 @@ public class Hello {
 		System.out.println("216481604 - This line was added by Omar Martinez.");
 		System.out.println("301814734 - This line was added by Omar Ibrahim.");
 		System.out.println("301603666 - This line was added by Pratham Saldhi. ");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("301187549 - This line was added by Sam Ho.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
